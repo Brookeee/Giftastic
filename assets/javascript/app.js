99c4e8bad59458b2e5d3407e56af5414ded2d903
Brookee <oliver.brooke.a@gmail.com>
2019-06-03 22:21:23 -0400

    buttons fixed, addtl changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,23 @@
 $(document).ready(function() {
-    var era = ["movies", "muisc", "tv", "kid", "90s"];
+    var topics = ["Movies", "Muisc", "TV", "Kid", "90s"];
+
+// Giphy subject buttons. Create DRY code for buttons/Current code works but not DRY
+function createBtns() {
+
+    $("#buttons").empty();
+    for (var i = 0; i < topics.length; i++) {
+        var b = $("<button>");
+        b.addClass("90s");
+        b.attr("data-name", topics[i]);
+        b.text(topics[i]);
+        $("#buttons").append(b);
+    };
+};
+
+    createBtns();
+
+    // function displayGifs() {
+    //     var topics = #(this).attr("data-era");
+    //     var queryURL = ""
+    // }
 });
