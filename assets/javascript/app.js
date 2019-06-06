82e9a68f16ca07bb407161b96288375293c12eeb
Brookee <oliver.brooke.a@gmail.com>
2019-06-05 22:24:16 -0400

    WIP2

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,23 +1,67 @@
 $(document).ready(function() {
-    var topics = ["Movies", "Muisc", "TV", "Kid", "90s"];
+    var topics = ["The Nanny", "Clueless", "Rugrats"];
 
-// Giphy subject buttons. Create DRY code for buttons/Current code works but not DRY
+// Giphy subject buttons. **FIXED**
 function createBtns() {
 
     $("#buttons").empty();
     for (var i = 0; i < topics.length; i++) {
         var b = $("<button>");
-        b.addClass("90s");
+        b.addClass("topic");
         b.attr("data-name", topics[i]);
         b.text(topics[i]);
         $("#buttons").append(b);
-    };
-};
-
+    }
+}
     createBtns();
 
-    // function displayGifs() {
-    //     var topics = #(this).attr("data-era");
-    //     var queryURL = ""
-    // }
+    $("#add-era").on("click", function(){
+
+        var newEra = $("#era-input").val().trim();
+        topics.push(newEra);
+        createBtns();
+    });
+    // function to display gifs 
+    function showGifs() {
+        
+        var second = $(this).attr("data-name");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + second + "&limit=5&api_key=exOswkiLF8WPK8waIHmRr9HEwBjHNnkm";
+
+        // ajax call
+        $.ajax ({
+            url: queryURL,
+            method: "GET"
+        })
+        .done (function (response){
+            console.log(response.data);
+
+            // animate/still 
+            $("#buttons-view").on("click", ".btn-primary", function() {
+            var results = response.data;
+            for (var i = 0; i < results.length; i++) {
+                var newDiv = $('<div>');
+                var goGif = $("<img class = 'gif' data-state = 'animate'>");
+                goGif.attr('src', results[i].images.fixed_width_url);
+                goGif.attr("data-animate", results[i].images.fixed_width.url);
+                goGif.attr("data-still", results[i].images.fixed_still.url);
+
+                // rating 
+                var gifRating = $("<p>")
+
+                gifRating.text("Rated: PG-13 " + results[i].gifRating);
+
+                console.log(gifRating);
+
+                newDiv.prepend(goGif, gifRating);
+                $("#90s").prepend(newDiv);
+                console.log(response);
+            }
+        })
+        
+        
+        });
+    }
+
+    // call to function to show gifts 
+    showGifs();
 });
